fdc5e95a20173b797e54b0fe7588869715a3a211
Mael-14 <robertmael143@gmail.com>
2026-01-05 17:20:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
-require('dotenv').config({ path: '.evn' });
+require('dotenv').config({ path: '.env' });
 
 const app = express();
 const server = http.createServer(app);
